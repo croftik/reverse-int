039b97aef22f38ae936238d3258c87bb5a9fc39b
croftik <Chayka_Lory@mail.ru>
2020-09-30 23:46:06 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) n = n * (-1);
+    let str = String(n);
+    let i = str.length - 1;
+    let res = '';
+    while (0 <= i){
+      res = `${res}${str[i]}`;
+      i = i - 1;
+    }
+    return Number(res);
 }
